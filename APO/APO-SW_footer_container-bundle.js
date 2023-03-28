7474f2e6a452aa2d91b34474e0f18011256e9df7
aalkardali-blm <aalkardali@blm-alliance.com>
2023-03-28 11:18:34 +0200

    update cdn

--- APO/APO-SW_footer_container-bundle.js
+++ APO/APO-SW_footer_container-bundle.js
@@ -1,1 +1,1 @@
-!function(){const e=document.createElement("script");e.type="text/javascript",e.src="https://cdn.jsdelivr.net/gh/blh-sa/pSearchParams@master/dist/APO/APO-SW_preservparam-bundle.js",c=document.getElementById("footer"),c.parentNode.insertBefore(e,c)}();
+!function(){const e=document.createElement("script");e.type="text/javascript",e.src="https://cdn.jsdelivr.net/gh/blh-sa/pSearchParams@2023.13/dist/APO/APO-SW_preservparam-bundle.js",c=document.getElementById("footer"),c.parentNode.insertBefore(e,c)}();
