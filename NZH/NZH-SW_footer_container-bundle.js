b10e6ac92bd032c29c4062b703a9daec2b343a48
aalkardali-blm <aalkardali@blm-alliance.com>
2023-03-29 13:34:38 +0200

    update url cdn

--- NZH/NZH-SW_footer_container-bundle.js
+++ NZH/NZH-SW_footer_container-bundle.js
@@ -1,1 +1,1 @@
-!function(){const e=document.createElement("script");e.type="text/javascript",e.src="https://cdn.jsdelivr.net/gh/blh-sa/pSearchParams@2023.13/dist/NZH/NZH-SW_preservparam-bundle.js",c=document.getElementById("footer"),c.parentNode.insertBefore(e,c)}();
+!function(){const e=document.createElement("script");e.type="text/javascript",e.src="https://cdn.jsdelivr.net/gh/blh-sa/pSearchParams@2023.13/NZH/NZH-SW_preservparam-bundle.js",c=document.getElementById("footer"),c.parentNode.insertBefore(e,c)}();
