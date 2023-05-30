2dcd236e5c04bf1ab5cbdc266b86699dd2165600
aalkardali-blm <aalkardali@blm-alliance.com>
2023-05-30 11:09:27 +0200

    change URL sentry

---
+++ NVHDE/NVHDE-LP_header_container-bundle.js
@@ -0,0 +1,1 @@
+!function(){c=document.getElementById("header");const e=document.createElement("script");e.type="text/javascript",e.src="https://js.sentry-cdn.com/dfc8c8ff7a2a4d87add4091670a339aa.min.js",c.parentNode.insertBefore(e,c),Sentry.onLoad((function(){Sentry.configureScope((e=>{Sentry.setTag("SW","NVHDE")}))}))}();
